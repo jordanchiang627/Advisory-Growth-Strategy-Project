da8fc80932526cd655e9ad35d1aa7d2874ba4575
Jordan <jchian@uw.edu>
2025-01-25 15:30:40 -0800

    demographics look

--- DataCleaningR3.py
+++ DataCleaningR3.py
@@ -6,6 +6,13 @@ def save_csv(df, file_path):
     df.to_csv(file_path, index = False)
     print(f"DataFrame successfully saved to {file_path}")
 
+def age_proportions(df, col1, col2, new_column):
+    """
+    This function will take column 1 and divide it by column 2 to find the wanted proportion 
+    """
+    df[new_column] = (df[col1]/df[col2].replace(0, pd.NA)).round(4)
+    return df
+
 demographics = pd.read_csv("CleanedDataR2/demogrpahics_r2.csv")
 
 demographics["Dependants"] = (demographics["Under 5 years"] + demographics["5 to 9 years"] + demographics["10 to 14 years"] + 
@@ -21,9 +28,16 @@ demographics["Primary Stage Investors"] = (demographics["35 to 44 years"] + demo
 demographics["55 to 59 years"] + demographics["60 to 64 years"])
 demographics["Early Stage Investors"] = demographics["25 to 34 years"]
 
+age_proportions(demographics, "Dependants", "Total population", "Dependants Proportion")
+age_proportions(demographics, "Working Age", "Total population", "Working Age Proportion")
+age_proportions(demographics, "Retired", "Total population", "Retired Proportion")
+age_proportions(demographics, "Early Stage Investors", "Working Age", "Early Stage Proportion of Working Age")
+age_proportions(demographics, "Primary Stage Investors", "Working Age", "Primary Stage Proportion of Working Age")
 
-demographics = demographics[['Geography', 'Geographic Area Name', 'Total population', 'Dependants', 'Early Stage Investors', 'Primary Stage Investors',
-'Working Age', 'Retired']]
+demographics = demographics[['Geography', 'Geographic Area Name', 'Total population', 'Dependants', 'Dependants Proportion', 'Early Stage Investors', 'Early Stage Proportion of Working Age',
+'Primary Stage Investors', 'Primary Stage Proportion of Working Age', 'Working Age', 'Working Age Proportion', 'Retired', 'Retired Proportion']]
+
+
 
 save_csv(demographics, "CleanedDataR3/demographics_r3.csv")
 
